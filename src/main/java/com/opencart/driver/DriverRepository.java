15b37d9d8fb43156015aa752c6dd7c7cf79203eb
Alex <oles.mob1@gmail.com>
2021-04-20 21:18:51 +0300

    First Commit

---
+++ src/main/java/com/opencart/driver/DriverRepository.java
@@ -0,0 +1,31 @@
+package com.opencart.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverRepository {
+
+    public static final ThreadLocal<WebDriver> DRIVERS = new ThreadLocal<>();
+    private static WebDriver webDriver;
+
+    public DriverRepository() {
+    }
+
+    public static void downloadWebDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    public static void instanceWebBrowser() {
+        webDriver = new ChromeDriver();
+        DRIVERS.set(webDriver);
+    }
+
+    public static void closeBrowser() {
+        webDriver.quit();
+    }
+
+    public static WebDriver getWebDriver() {
+        return webDriver;
+    }
+}
